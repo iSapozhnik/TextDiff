e53233e85b260163a7a856f16957e7f91b7d2689
Ivan Sapozhnik <sapozhnik.ivan@gmail.com>
2026-02-17 15:12:43 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TextDiff",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TextDiff",
+            targets: ["TextDiff"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "TextDiff"),
+        .testTarget(
+            name: "TextDiffTests",
+            dependencies: ["TextDiff"]
+        ),
+    ]
+)
